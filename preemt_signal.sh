c1852d86f248a2d5358031c878acb1835ac54bc2
matthias <matthias.anderer@googlemail.com>
2020-09-30 12:47:08 +0200

    Create preemt_signal.sh

---
+++ preemt_signal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+META_URL="http://metadata.google.internal/computeMetadata/v1/instance"
+GCLOUD=gcloud
+TOPIC="prere"
+get_meta() {
+  curl -s "$META_URL/$1" -H "Metadata-Flavor: Google"
+}
+IS_PREEMPTED="$( get_meta preempted )"
+if [ "$IS_PREEMPTED" == "TRUE" ]; then
+  NAME="$( get_meta name )"
+  ZONE="$( get_meta zone | cut -d '/' -f 4 )"
+  "$GCLOUD" pubsub topics publish "$TOPIC" \
+      --message '{"name": "'${NAME}'", "zone": "'${ZONE}'"}'
+fi
